0ca8d4a6df7d8337bce615dc6ceaf6c70a590ad6
rabbiigot <147376032+rabbiigot@users.noreply.github.com>
2025-03-31 21:51:02 +0800

    update server.js

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.use(cors({
   credentials: true, 
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+  next(); 
+});
+
 app.use(cookieParser());
 
 const upload = multer({ dest: 'uploads/' });
